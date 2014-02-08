659fa5d7aac6b10e7889968600e524e45ce500b7
Justin Woo <moomoowoo@gmail.com>
2014-02-07 23:58:42 -0600

    still lernin'

--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@ App = Ember.Application.create();
 
 App.Router.map(function () {
   this.resource('about');  
-  this.resource('shows');
+  this.resource('shows', function () {
+    this.resource('editshow', { path: 'edit/:show_id' })
+  });
 });
 
+App.ShowsRoute = Ember.Route.extend({
+  model: function(params) {
+    if (params == null) {
+      return shows;
+    } else {
+      return shows.findBy('id', params.show_id);
+    }
+  }
+});
+
+
 var shows = [{
-  
-}]
+  id: '1',
+  title: '俺のお嫁さんと一心同体',
+  episode: '1'
+}];
