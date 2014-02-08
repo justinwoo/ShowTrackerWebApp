34cfa426cb7413c8f0c5f4da9f193d76234f3167
Justin Woo <moomoowoo@gmail.com>
2014-02-08 01:41:17 -0600

    i like to pretend i actually did something but really i didnt do anything

--- js/app.js
+++ js/app.js
@@ -3,20 +3,29 @@ App = Ember.Application.create();
 App.Router.map(function () {
   this.resource('about');  
   this.resource('shows', function () {
-    this.resource('editshow', { path: 'edit/:show_id' })
+    this.resource('editshow', { path: 'edit/:show_id' });  
   });
 });
 
 App.ShowsRoute = Ember.Route.extend({
-  model: function(params) {
-    if (params == null) {
-      return shows;
-    } else {
-      return shows.findBy('id', params.show_id);
-    }
+  model: function() {
+    return shows;
   }
 });
 
+App.EditshowRoute = Ember.Route.extend({
+  model: function(params) {
+    return shows.findBy('id', params.show_id);
+  }
+})
+
+App.EditshowController = Ember.ObjectController.extend({
+  actions: {
+    doneEditing: function () {
+      //do something
+    }
+  }
+});
 
 var shows = [{
   id: '1',
